9b9c6075a0a94be5c56cecba4e07e0efe80011cd
lucasatwork2026 <lucasatwork2026@gmail.com>
2026-09-18 17:53:27 -0400

    Test revised pre-news schedule

--- test_scheduled_flatten.py
+++ test_scheduled_flatten.py
@@ -5,16 +5,16 @@ from strategy_scheduled_flatten import flatten_tick_due
 
 class ScheduledFlattenTests(unittest.TestCase):
     def test_exact_flatten_ticks(self):
-        self.assertEqual(flatten_tick_due(74, set()), (74, 75))
-        self.assertEqual(flatten_tick_due(149, {74}), (149, 150))
-        self.assertEqual(flatten_tick_due(224, {74, 149}), (224, 225))
+        self.assertEqual(flatten_tick_due(73, set()), (73, 74))
+        self.assertEqual(flatten_tick_due(148, {73}), (148, 149))
+        self.assertEqual(flatten_tick_due(223, {73, 148}), (223, 224))
 
     def test_one_tick_catch_up(self):
-        self.assertEqual(flatten_tick_due(75, set()), (74, 75))
+        self.assertEqual(flatten_tick_due(74, set()), (73, 74))
 
     def test_completed_or_late_ticks_do_not_repeat(self):
-        self.assertIsNone(flatten_tick_due(75, {74}))
-        self.assertIsNone(flatten_tick_due(76, set()))
+        self.assertIsNone(flatten_tick_due(74, {73}))
+        self.assertIsNone(flatten_tick_due(75, set()))
 
 
 if __name__ == "__main__":
